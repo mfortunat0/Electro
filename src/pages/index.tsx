e54e54417405fe3597ce3ebc71daba70a39edee5
matheus <matheus.xmaz10@gmail.com>
2021-05-17 10:45:32 -0300

    Base home navbar completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,51 @@
+import { Button, Nav } from "../styles/index";
+import {
+  FaHeart,
+  FaGamepad,
+  FaPlug,
+  FaBabyCarriage,
+  FaCouch,
+  FaLandmark,
+  FaMask,
+  FaShopify,
+} from "react-icons/fa";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Nav>
+        <Button>
+          <FaPlug size={16} /> &nbsp; Eletrônicos
+        </Button>
+        <Button>
+          <FaGamepad size={18} />
+          &nbsp; Games e PC Gamer
+        </Button>
+        <Button>
+          <FaCouch size={16} />
+          &nbsp; Casa e Cozinha
+        </Button>
+        <Button>
+          <FaShopify size={16} />
+          &nbsp; Grátis
+        </Button>
+        <Button>
+          <FaMask size={16} />
+          &nbsp; Moda
+        </Button>
+        <Button>
+          <FaHeart size={16} />
+          &nbsp; Saúde e Beleza
+        </Button>
+        <Button>
+          <FaBabyCarriage size={16} />
+          &nbsp; Família e Crianças
+        </Button>
+        <Button>
+          <FaLandmark size={16} />
+          &nbsp; Finanças
+        </Button>
+      </Nav>
+    </>
   );
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #1d1f20;
+  height: 64px;
+  padding: 0 5.8vw;
+`;
+
+export const Button = styled.button`
+  color: #fff;
+  outline: unset;
+  border: unset;
+  border-radius: 4px;
+  background-color: #34373b;
+  font-size: 12px;
+  padding: 10px 14px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  :hover {
+    background-color: #595d62;
+  }
+`;
