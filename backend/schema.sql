c2701153fceeb962873802e5464c5745b9be5dc9
matheus <matheus.xmaz10@gmail.com>
2021-06-11 20:15:10 -0300

    add schema for database

---
+++ backend/schema.sql
@@ -0,0 +1,14 @@
+create database raio;
+create table users(
+  id serial primary key,
+  email varchar(100),
+  pass varchar(100)
+);
+create table oferts(
+  id serial primary key,
+  title varchar(100),
+  value float,
+  description varchar(100),
+  company varchar(100),
+  user_name varchar(100)
+)
