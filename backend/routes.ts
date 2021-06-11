8455fe697790c9146c7781eec740f0d89f4844ed
matheus <matheus.xmaz10@gmail.com>
2021-06-11 20:58:04 -0300

    add features add user and get user

--- backend/routes.ts
+++ backend/routes.ts
@@ -3,12 +3,49 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const router = Router();
 
-const findUserByEmailPassword = async (email: string, password: string) => {
-  prisma.$connect();
+const findUserByEmailPassword = async (email: string, pass: string) => {
+  return await prisma.users.findFirst({
+    where: {
+      email,
+      pass,
+    },
+  });
 };
 
+const createNewUser = async (email: string, pass: string) => {
+  return await prisma.users.create({
+    data: {
+      email,
+      pass,
+    },
+  });
+};
 router.get("/", (req, res) => res.send("Hello"));
-router.get("/users", (req, res) => {});
-router.post("/users", (req, res) => {});
+router.get("/users", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await findUserByEmailPassword(email, password)
+    .catch((error) => console.error(error))
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+  if (user) {
+    res.json(user).status(200);
+  } else {
+    res
+      .json({
+        message: "User not found",
+      })
+      .status(404);
+  }
+});
+router.post("/users", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await createNewUser(email, password)
+    .catch((error) => console.error(error))
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+  res.json(user).status(200);
+});
 
 export default router;
